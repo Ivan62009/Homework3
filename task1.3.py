16424849970a58dbe0be7bd65b66dd40be8978d8
Ivan62009 <140536861+Ivan62009@users.noreply.github.com>
2023-07-28 15:40:37 +0300

    Initial commit

---
+++ task1.3.py
@@ -0,0 +1,12 @@
+prompt = 'Pick a random number between 1 and 10.'
+print('Pick a random number between 1 and 10.')
+number = input('Your number is: ')
+import random
+print('My number is')
+print(random.randint(1, 10))
+if number == random.randint:
+    print('You won!')
+if number > '10':
+    print('Hey it`s not fair!')
+elif number != random.randint:
+    print('You lost.')
---
+++ task2.3.py
@@ -0,0 +1,3 @@
+name = 'Ivan'
+my_age = 17
+print('Hello' + ' ' + name + ' ' + ', on your next birthday you’ll be' + ' ' + str(my_age + 1) + ' ' + 'years old!')
